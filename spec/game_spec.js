e1234450319486281e7c6fc8fc0f5597b9f9f5b0
tnaughts <tnaughts@gmail.com>
2016-09-21 13:49:13 -0500

    update testing for custom array

--- spec/game_spec.js
+++ spec/game_spec.js
@@ -1,7 +1,11 @@
-describe("an initial default game", function(){
+describe("Setting up and initial game", function(){
 	var game;
 	it("has a defualt game setup", function(){
 		game = new Game();
 		expect(game.gameArray).toEqual([[0,0,0,0], [2,0,2,0], [0,0,0,0], [0,0,0,0]]);
+	})
+	it("can have a custom game setup", function(){
+		game = new Game("0000202000000022");
+		expect(game.gameArray).toEqual([[0,0,0,0], [2,0,2,0], [0,0,0,0], [0,0,2,2]])
 	})
 })
